1cd13a2bacd26c5ea75b43f8b3b44c2058bece15
ryszardpanda <rychu48@op.pl>
2025-04-28 19:34:24 +0200

    + spring initalization

---
+++ src/main/java/com/UserInteraction/UserInteractionMicroService/UserInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.UserInteraction.UserInteractionMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInteractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UserInteraction/UserInteractionMicroService/UserInteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserInteraction.UserInteractionMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInteractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
